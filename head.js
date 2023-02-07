8d3edf922bd72430fe6f3595a274522396ed5dfe
Spencer Hahmo <schahmo@gmail.com>
2023-02-07 12:44:47 -0700

    Correctly asserts if the header is undefined

--- head.js
+++ head.js
@@ -1,8 +1,10 @@
 let header = undefined;
 
 const head = function(array) {
-  if (array.length > 0) {
+  if (array.length !== undefined) {
     header = array[0];
+  } else if (array[0] === undefined) {
+    header = undefined;
   }
 };
 
@@ -17,4 +19,4 @@ const assertEqual = function(array, expectedValue) {
 assertEqual(head([5,6,7]), 5);
 assertEqual(head([1,2,3,4]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Lighthouse");
+assertEqual(head([]), "Labbing");
