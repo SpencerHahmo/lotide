a721e743c4a170b05e1ff7ea2660e5244cd3ec8e
Spencer Hahmo <schahmo@gmail.com>
2023-02-07 21:57:27 -0700

    Removes values from an array if it matches with the values you want removed

---
+++ without.js
@@ -0,0 +1,26 @@
+const words = ["hello", "world", "lighthouse"];
+
+const without = function(source, itemsToRemove) {
+  let arrayOfValues = [];
+  for (let i = 0; i < source.length; i++) {
+    arrayOfValues.push(source[i]);
+  }
+  for (let i = 0; i <= itemsToRemove.length; i ++) {
+    const removeIndex = arrayOfValues.indexOf(itemsToRemove[i]);
+    if (removeIndex > -1) {
+      arrayOfValues.splice(removeIndex, 1);
+    }
+  }
+  console.log(arrayOfValues);
+};
+
+
+without([1, 2, 3], [1]); // Should be [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]); // Should be ["1", "2"]
+without(["Testing", "with", "nothing", "to", "remove"],[]);
+without([1, "2", '3', false, true, "jackpot"], ["1", '2', '3', false, undefined, NaN]);
+without(["its", "over", "9000", false, 9001, "!", true], ["9000", false, true, undefined, NaN]);
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+without(words, ["hello", "world", "lighthouse"]);
+without(["hello", "world", "lighthouse", "outstanding", "job"], words)
