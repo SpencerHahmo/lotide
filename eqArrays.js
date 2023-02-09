7ea84928c6d3b6a6261ae17da9a82bad14c061ca
Spencer Hahmo <schahmo@gmail.com>
2023-02-08 18:54:44 -0700

    Cleaned up the code and added additional test cases

--- eqArrays.js
+++ eqArrays.js
@@ -1,3 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
 const eqArrays = function(actual, expected, expectedResult) {
   let check = false;
   for (let i = 0; i < actual.length; i++) {
@@ -11,24 +19,30 @@ const eqArrays = function(actual, expected, expectedResult) {
   if (expectedResult === undefined) {
     if (check === true) {
       console.log("true");
+      return true;
     } else if (check === false) {
       console.log("false");
+      return false;
     }
   } else if (expectedResult !== undefined) {
     if (check === expectedResult) {
       console.log("true");
+      return true;
     } else if (check !== expectedResult) {
       console.log("false");
+      return false;
     }
   }
 };
 
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
 
-eqArrays([1, 2, 3], [1, 2, 3], true); // => should PASS
-eqArrays(["1", "2", 3], ["1", "2", 3], false); // => should FAIL
-eqArrays([1, 2, 3], [1, 3, 3], false); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, 2]), true); // => should FAIL
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), false); // => should FAIL
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should PASS
