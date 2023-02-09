9c113890b17320623de177ab5c0a1e3ee4b62348
Spencer Hahmo <schahmo@gmail.com>
2023-02-08 19:18:11 -0700

    Cleaned up the code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,4 @@
-const assertArraysEqual = function(actual, expected, expectedResult) {
+const eqArrays = function(actual, expected) {
   let check = false;
   for (let i = 0; i < actual.length; i++) {
     if (actual[i] === expected[i] && typeof actual[i] === typeof expected[i]) {
@@ -8,35 +8,39 @@ const assertArraysEqual = function(actual, expected, expectedResult) {
       break;
     }
   }
-  if (expectedResult === undefined) {
-    if (check === true) {
-      console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}\n`);
-    } else if (check === false) {
-      console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}\n`);
+  if (check === true) {
+    console.log("true");
+    return true;
+  } else {
+    console.log("false");
+    return false;
+  }
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (actual === expected) {
+    if (expected === true) {
+      console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
     }
-  } else if (expectedResult !== undefined) {
-    if (check === expectedResult) {
-      if (expectedResult === true) {
-        console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}\n`);
-      } else {
-        console.log(`✅✅✅ Assertion Passed: ${actual} !== ${expected}\n`);
-      }
-    } else if (check !== expectedResult) {
-      if (expectedResult === true) {
-        console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}\n`);
-      } else {
-        console.log(`❌❌❌ Assertion Failed: ${actual} === ${expected}\n`);
-      }
+  } else {
+    if (expected === true) {
+      console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+    } else {
+      console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
     }
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
 
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
 
-assertArraysEqual([1, 2, 3], [1, 2, 3], true); // => should PASS
-assertArraysEqual(["1", "2", 3], ["1", "2", 3], false); // => should FAIL
-assertArraysEqual([1, 2, 3], [1, 3, 3], false); // => should PASS
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 2]), true); // => should FAIL
+
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), false); // => should FAIL
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should PASS
--- eqArrays.js
+++ eqArrays.js
@@ -25,11 +25,11 @@ const eqArrays = function(actual, expected) {
   }
 };
 
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 assertEqual(eqArrays([1, 2, 3], [1, 2, 2]), true); // => should FAIL
