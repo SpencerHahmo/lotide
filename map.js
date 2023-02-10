c9ce333073dc6e4360cc6223a78c650bac449878
Spencer Hahmo <schahmo@gmail.com>
2023-02-09 22:44:15 -0700

    Code that implements the .map function in various ways

---
+++ map.js
@@ -0,0 +1,34 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const assertArraysEqual = (actual, expected) => {
+  if (eqArrays(actual, expected) === true) console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  else console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const eqArrays = (actual, expected) => {
+  if (actual.length !== expected.length) return false;
+  for (let i = 0; i < actual.length; i++) {
+    if (Array.isArray(actual[i]) && (eqArrays(actual[i], expected[i]) === false)) return false;
+    else if (!(Array.isArray(actual[i])) && actual[i] !== expected[i]) return false;
+  }
+  return true;
+};
+
+const map = (array, callback) => {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+;}
+
+const results1 = map(words, word => word[0]);
+const results2 = map(words, word => word);
+const results3 = map(words, word => word.slice(0, 1));
+const resultsLength = map(words, word => word.length);
+
+assertArraysEqual(results1, ["h", "e", "l", "l", "o"]);
+assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']);
+assertArraysEqual(results2, ["ground", "control", "to", "major", "tom"]);
+assertArraysEqual(results3, ['g', 'c', 't', 'm', 't']);
+assertArraysEqual(resultsLength, [6, 7, 2, 5, 3]);;
